6e9cba22450e7886c9ae74bbfdeb82ef6d5baf2d
Prajeen Govardhanam <contact@prajeen.com>
2024-04-02 23:55:32 +0530

    :sparkles: Add small demo with kong and httprouter

--- cmd/objekt/main.go
+++ cmd/objekt/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/alecthomas/kong"
+	"github.com/julienschmidt/httprouter"
+)
+
+type CLI struct {
+	Hostname string `help:"Hostname of the Objekt server" default:"localhost" short:"H"`
+	Port     int    `help:"Port of the Objekt server" default:"8080" short:"p"`
+}
 
 func main() {
-  fmt.Println("Hello, Objekt!")
+	cli := CLI{}
+	kong.Parse(&cli)
+
+	router := httprouter.New()
+	router.GET("/objekt", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(w, "Objekt Server")
+	})
+
+	listener := fmt.Sprintf("%s:%d", cli.Hostname, cli.Port)
+	fmt.Printf("Starting Objekt Server on http://%s\n", listener)
+	http.ListenAndServe(listener, router)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go.prajeen.com/objekt
 
 go 1.22.1
+
+require (
+	github.com/alecthomas/kong v0.9.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
