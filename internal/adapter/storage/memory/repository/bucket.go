bce8b2cc964dbe6a49e239a8da1e871afdc09ca5
Prajeen Govardhanam <contact@prajeen.com>
2024-04-10 22:56:26 +0530

    :zap: Improve concurrency of in-memory database

--- internal/adapter/storage/memory/repository/bucket.go
+++ internal/adapter/storage/memory/repository/bucket.go
@@ -12,14 +12,12 @@ import (
 )
 
 type BucketRepository struct {
-	l *sync.Mutex
-	m map[uuid.UUID]*domain.Bucket
+	m sync.Map
 }
 
 func NewBucketRespository() *BucketRepository {
 	return &BucketRepository{
-		l: &sync.Mutex{},
-		m: make(map[uuid.UUID]*domain.Bucket),
+		m: sync.Map{},
 	}
 }
 
@@ -35,49 +33,47 @@ func (b *BucketRepository) CreateBucket(ctx context.Context, bucket *domain.Buck
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	b.l.Lock()
-	defer b.l.Unlock()
-	b.m[preparedBucket.ID] = preparedBucket
+	b.m.Store(preparedBucket.ID, preparedBucket)
 	return preparedBucket, nil
 }
 
 func (b *BucketRepository) GetBucketByID(ctx context.Context, id uuid.UUID) (*domain.Bucket, error) {
-	b.l.Lock()
-	defer b.l.Unlock()
-	bucket, ok := b.m[id]
+	bucket, ok := b.m.Load(id)
 	if ok {
-		return bucket, nil
+		return bucket.(*domain.Bucket), nil
 	}
 	return nil, fmt.Errorf("bucket with id=%s doesn't exist", id.String())
 }
 
 func (b *BucketRepository) GetBucketByName(ctx context.Context, name string) (*domain.Bucket, error) {
-	b.l.Lock()
-	defer b.l.Unlock()
-	for _, v := range b.m {
+	var bucket *domain.Bucket
+	b.m.Range(func(key, value any) bool {
+		v := value.(*domain.Bucket)
 		if v.Name == name {
-			return v, nil
+			bucket = v
+			return false
 		}
+		return true
+	})
+	if bucket != nil {
+		return bucket, nil
 	}
 	return nil, fmt.Errorf("bucket with name=%s doesn't exist", name)
 }
 
 func (b *BucketRepository) ListBuckets(ctx context.Context) ([]domain.Bucket, error) {
-	buckets := make([]domain.Bucket, 0, len(b.m))
-	b.l.Lock()
-	defer b.l.Unlock()
-	for _, b := range b.m {
-		buckets = append(buckets, *b)
-	}
+	buckets := make([]domain.Bucket, 0)
+	b.m.Range(func(key, value any) bool {
+		buckets = append(buckets, *value.(*domain.Bucket))
+		return true
+	})
 	return buckets, nil
 }
 
 func (b *BucketRepository) DeleteBucket(ctx context.Context, id uuid.UUID) error {
-	_, ok := b.m[id]
+	_, ok := b.m.Load(id)
 	if ok {
-		b.l.Lock()
-		defer b.l.Unlock()
-		delete(b.m, id)
+		b.m.Delete(id)
 		return nil
 	}
 	return fmt.Errorf("bucket with id=%s doesn't exist", id.String())
