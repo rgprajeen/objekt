003f6636f67a51bd318ef64ed34b2a5f27ac20c4
Prajeen Govardhanam <contact@prajeen.com>
2024-04-11 23:58:09 +0530

    :construction: Add initial sql migration scripts

---
+++ internal/adapter/storage/postgres/migrations/00001_CreateBucket.down.sql
@@ -0,0 +1,3 @@
+drop table if exists bucket;
+drop type if exists bucket_type_enum;
+drop type if exists bucket_region_enum;
---
+++ internal/adapter/storage/postgres/migrations/00001_CreateBucket.up.sql
@@ -0,0 +1,15 @@
+create type bucket_type_enum as enum ('aws', 'azure', 'oci');
+create type bucket_region_enum as enum ('ashburn', 'frankfurt', 'london', 'phoenix', 'singapore', 'sydney');
+
+create table bucket (
+  id bigserial primary key,
+  public_id uuid not null default uuid_generate_v4(),
+  name text not null,
+  region bucket_region_enum not null,
+  type bucket_type_enum not null,
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+
+create unique index bucket_public_id_uindex on bucket (public_id);
+create unique index bucket_name_uindex on bucket (name);
